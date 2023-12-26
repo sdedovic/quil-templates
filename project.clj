fe7f87245ad92ad90d7da30fc136096ab0c6bf06
sdedovic <stevan@dedovic.com>
2023-12-26 21:26:18 +0000

    update for deploy

--- project.clj
+++ project.clj
@@ -3,6 +3,18 @@
   :url "https://github.com/sdedovic/quil-templates"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :deploy-repositories [["releases"  {:sign-releases false :url "https://clojars.org"}]
-                        ["snapshots" {:sign-releases false :url "https://clojars.org"}]]
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version"
+                   "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "--no-sign"] ;; TODO: start signing things
+                  ["deploy"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]]
+  :deploy-repositories [["releases" config 
+                         {:sign-releases false 
+                          :url "https://clojars.org"
+                          :username :env/clojars_user
+                          :password :env/clojars_token}]]
   :eval-in-leiningen true)
