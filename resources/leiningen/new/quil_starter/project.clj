9d2bc7b7cd8d0f71da3dbe65d388dc2c0d5880f8
sdedovic <stevan@dedovic.com>
2024-01-10 23:29:23 +0000

    upgrade genartlib

--- resources/leiningen/new/quil_starter/project.clj
+++ resources/leiningen/new/quil_starter/project.clj
@@ -5,7 +5,7 @@
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [com.dedovic/artlib-core "0.0.10"]
-                 [genartlib/genartlib "0.1.21"]] ; utility functions
+                 [genartlib/genartlib "1.0.0"]] ; utility functions
   :jvm-opts ["-Xms4000m" "-Xmx4000M" ; 4GB heap size
              "-server"
              "-Dsun.java2d.uiScale=1.0"] ; adjust scaling for high DPI displays
