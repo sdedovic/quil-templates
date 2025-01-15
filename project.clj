51b0672ee3c6a0862a0ff44af93a904aa626dc1d
sdedovic <stevan@dedovic.com>
2025-01-15 02:22:34 +0000

    Version 0.1.13-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.dedovic/lein-template.quil-starter "0.1.12"
+(defproject com.dedovic/lein-template.quil-starter "0.1.13-SNAPSHOT"
   :description "Leiningen template for a Quil starter project."
   :url "https://github.com/sdedovic/quil-templates"
   :plugins [[lein-changelog "0.3.2"]]
